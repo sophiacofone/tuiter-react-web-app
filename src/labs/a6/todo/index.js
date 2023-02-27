92507f410a0955be7e50646d118fe0f3fcbabd0e
sophiacofone <sophiacofone@gmail.com>
2023-02-27 13:22:35 -0500

    lab 6

---
+++ src/labs/a6/todo/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList
+    from "./todo-list";
+
+const ToDo = () => {
+    return(
+        <>
+            <TodoList/>
+        </>
+    );
+};
+export default ToDo;
